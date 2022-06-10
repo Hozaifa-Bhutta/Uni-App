e0b0a4622cd450469952e8b7c56c580ca39a7bb4
Hozaifa <46136202+Hozaifa-Bhutta@users.noreply.github.com>
2022-06-10 13:47:32 -0500

    Added a "divider" between tasks

--- Task.js
+++ Task.js
@@ -13,15 +13,19 @@ export default function Task(props) {
 
 
     if (!isCompleted) {
-        return <View style = {{width: "70%"}}>
-            <Text style = {{fontSize: 0.03 * height}}>
-                {props.title}
-            </Text>
-            <Text style = {{alignSelf: "center",fontSize: 0.02 * height }}>
-                Description: {props.description}
-            </Text>
-            <Button title = "Complete" onPress= {() => onPress()}></Button>
-        </View>
+        return <View style = {{height: "20%", width: "60%"}}>
+
+                <Text style = {{fontSize: 0.03 * height}}>
+                    {props.title}
+                </Text>
+                <Text style = {{alignSelf: "center",fontSize: 0.02 * height }}>
+                    Description: {props.description}
+                </Text>
+                <Button title = "Complete" onPress= {() => onPress()}></Button>
+                <View style = {{height: "10%"}}></View>
+
+
+            </View>
     } 
     else {
         return <View></View>
